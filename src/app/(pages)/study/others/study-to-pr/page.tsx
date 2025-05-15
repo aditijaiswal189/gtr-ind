80a2749f9270e4bd9b060609c03993b769354da6
Aditi Jaiswal <aditijaiswal189@gmail.com>
2025-05-15 09:26:00 +0530

    added 2 more pages

--- src/app/(pages)/study/others/study-to-pr/page.tsx
+++ src/app/(pages)/study/others/study-to-pr/page.tsx
@@ -1,15 +1,101 @@
 import { features } from "@/app/(pages)/about-us/about/about/page";
 import { CtaWithFeaturesRightImage } from "@/components/ui/cta-with-features-right-image";
+import { FeatureGrid4, FeatureItemData } from "@/components/ui/feature-grid4";
+import { Feature3 } from "@/components/ui/feature3";
+import {
+  FeatureItemPropsList,
+  FeatureList,
+} from "@/components/ui/features-list";
 import { Hero2 } from "@/components/ui/hero2";
+import { Briefcase, Heart, Notebook, Plane, Smile, Star } from "lucide-react";
 import React from "react";
 
+const items: FeatureItemPropsList[] = [
+  {
+    icon: Star,
+    text: " Enroll in a study program at a designated learning institution in Canada.",
+  },
+
+  {
+    icon: Briefcase,
+    text: "Ensure compliance with immigration and study regulations during your study period.Working under Post-Graduation Work Permit (PGWP)",
+  },
+  {
+    icon: Notebook,
+    text: "After completing your study program in Canada, apply for a Post-Graduation Work Permit (PGWP). PGWP allows you to work in Canada for a specific duration after graduation (8 months to 3 years, depending on your program).",
+  },
+  {
+    icon: Smile,
+    text: " Accumulating work experience in Canada can enhance your chances of obtaining permanent residency.",
+  },
+];
+const features2: FeatureItemData[] = [
+  {
+    title: "Federal Skilled Worker (FSW)",
+    description:
+      "Participate in the Canadian Express Entry system, where you compete with other candidates to receive an Invitation to Apply for permanent residency. Express Entry uses a ranking system based on various factors, including age, education, work experience, and language proficiency in English or French",
+    icon: <Plane />,
+  },
+  {
+    title: "Canadian Experience Class (CEC)",
+    description:
+      "If you have sufficient work experience in Canada and meet the criteria of the Canadian Experience Class program, you can apply for permanent residency through this pathway.CEC is often a favorable choice for individuals with Canadian work experience",
+    icon: <Notebook />,
+  },
+
+  {
+    title: "Provincial Nominee Program (PNP)",
+    description:
+      "If you intend to settle in a specific province or territory of Canada, consider the Provincial Nominee Program (PNP). Each province has its own unique requirements and criteria. If you are nominated through a PNP stream, you will have the opportunity to apply for provincial nomination and qualify for permanent residency",
+    icon: <Briefcase />,
+  },
+  {
+    title: "Other pathways to Canada PR",
+    description:
+      "Explore other immigration options, such as Family Sponsorship, the Start-Up Visa (SUV) program, and more. Consulting with GTR Worldwide to navigate the complex process of Canadian immigration and choose the best option for your personal circumstances and objectives.",
+    icon: <Heart />,
+  },
+];
+const features3 = [
+  {
+    icon: "Briefcase" as const,
+    title: "",
+    description: "",
+  },
+  {
+    icon: "Notebook" as const,
+    title: "",
+    description: "",
+  },
+  {
+    icon: "Layers" as const,
+    title: "",
+    description: "",
+  },
+  {
+    icon: "BriefcaseBusiness" as const,
+    title: "",
+    description: "",
+  },
+  {
+    icon: "Network" as const,
+    title: "",
+    description: "",
+  },
+  {
+    icon: "Map" as const,
+    title: "",
+    description: "",
+  },
+];
 const StudyToPR: React.FC = () => {
   return (
     <div className="min-h-screen bg-white flex gap-12 flex-col">
       <Hero2
         badgeText="GreenTech Resources"
-        title=""
-        description=""
+        title="Pathway from Study to PR"
+        description="
+Canada provides several pathways for international students to transition from study to permanent residency (PR)"
         imageUrl="/images/hero/i.avif"
         primaryCta={{
           label: "Study in Canada",
@@ -20,11 +106,35 @@ const StudyToPR: React.FC = () => {
           href: "/about-us/about/book-appointment",
         }}
       />
-
+      <FeatureList
+        badgeText="Completion of a Study Program in Canada "
+        heading={
+          <>
+            Common Pathway for International Students to Become
+            <span className="text-primary"> Permanent Residents in Canada</span>
+          </>
+        }
+        subheading="You should also demonstrate the benefit of their activities to Canada's culture, society, and economy and show groundwork in Canada, like establishing business contacts, accommodation, and business location for self-employed individuals."
+        items={items}
+      />
+      <FeatureGrid4
+        title="GET Permanent Residency through Popular Routes"
+        description="Find out more and book a consultation call with us."
+        features={features2}
+      />
+      <Feature3
+        badgeText="Study to PR"
+        heading="Where is it easy to apply for permanent residency for
+"
+        highlighted="post-graduate students?"
+        description="
+The competition in Canada’s Express Entry system is currently very high, and the required scores for receiving an Invitation to Apply (ITA) for permanent residency through the Canadian Experience Class (CEC) or Federal Skilled Worker (FSW) programs are quite competitive. Some provinces and territories in Canada offer Post-Graduation Immigration Programs specifically for international students. These programs typically require that international students have completed a study program in the respective province or territory and meet specific requirements. "
+        items={features3}
+      />
       <CtaWithFeaturesRightImage
-        badgeText="About Us"
+        badgeText="Pathway from Study to PR"
         title=" GreenTech Resource Worldwide Canada"
-        subtitle=""
+        subtitle="If you’re interested, consider reaching out to GTR Worldwide to learn more about the specific criteria for nomination"
         features={features}
       />
     </div>
--- src/app/(pages)/study/programs/designated-learning-institution(dli)/page.tsx
+++ src/app/(pages)/study/programs/designated-learning-institution(dli)/page.tsx
@@ -1,15 +1,54 @@
 import { features } from "@/app/(pages)/about-us/about/about/page";
 import { CtaWithFeaturesRightImage } from "@/components/ui/cta-with-features-right-image";
+import {
+  Feature2WithImage,
+  FeatureItemProps,
+} from "@/components/ui/features-2-with-image";
+import {
+  FeatureItemPropsList,
+  FeatureList,
+} from "@/components/ui/features-list";
 import { Hero2 } from "@/components/ui/hero2";
+import { Briefcase, FileText, Notebook, Star } from "lucide-react";
 import React from "react";
 
+const cards: FeatureItemProps[] = [
+  {
+    icon: <Briefcase />,
+    title: "Apply for a study permit",
+    description: ` To apply for a study permit, you need an acceptance letter from a designated learning institution. A designated learning institution is a school approved by a provincial or territorial government to host international students.
+All primary and secondary schools in Canada are designated learning institutions.`,
+  },
+
+  {
+    icon: <FileText />,
+    title: "Why do I need DLI Number?",
+    description: `Applicants must provide the Designated Learning Institution number (DLI #) for their school of choice. The DLI number is the number that begins with the letter “O” on the study permit application form.Sometimes, institutions may lose their designated status. Depending on when the status is lost, the international student may be affected.
+DLI`,
+  },
+];
+const items: FeatureItemPropsList[] = [
+  {
+    icon: Star,
+    text: " Students at a primary or secondary institution. These schools are all automatically designated, and no DLI number is provided.",
+  },
+  {
+    icon: Briefcase,
+    text: "- A family member of a foreign national whose application for a work permit or a study permit is approved in writing before the foreign national enters Canada. In this case, the family member may obtain an open study permit.",
+  },
+  {
+    icon: Notebook,
+    text: "Some institutions in Quebec are designated by the provincial government, but do not have a unique DLI number",
+  },
+];
 const DesignatedLearningInstitution: React.FC = () => {
   return (
     <div className="min-h-screen bg-white flex gap-12 flex-col">
       <Hero2
         badgeText="GreenTech Resources"
-        title=""
-        description=""
+        title="Designated Learning Institution (DLI)"
+        description="
+Apply for a study permit, you need an acceptance letter from a designated learning institution. A designated learning institution is a school approved by a provincial or territorial government to host international students."
         imageUrl="/images/hero/i.avif"
         primaryCta={{
           label: "Study in Canada",
@@ -20,11 +59,35 @@ const DesignatedLearningInstitution: React.FC = () => {
           href: "/about-us/about/book-appointment",
         }}
       />
+      <Feature2WithImage
+        heading={
+          <>
+            Designated Learning Institution
+            <span className="text-primary pl-2">(DLI)</span>
+          </>
+        }
+        subheading="Book a consultation today to explore immigration options and secure your family's future in Canada."
+        features={cards}
+        imageSrc="https://images.pexels.com/photos/927022/pexels-photo-927022.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+        imageAlt="Fox in the wild"
+      />
+      <FeatureList
+        badgeText="Study"
+        heading={
+          <>
+            Designated Learning Institution
+            {/* <br className="hidden md:block" /> */}
+            <span className="text-primary">(DLI) </span>
+          </>
+        }
+        subheading="In certain cases, students may be exempt from providing a letter from a DLI, or from providing a DLI number in their application. These cases may include, but are not limited to"
+        items={items}
+      />
 
       <CtaWithFeaturesRightImage
-        badgeText="About Us"
+        badgeText="Designated Learning Institution (DLI)"
         title=" GreenTech Resource Worldwide Canada"
-        subtitle=""
+        subtitle="Immigration policies can change over time, so it’s essential to stay updated with us for the latest information from the respective provincial or territorial immigration authorities. Consult GreenTech Resources Worldwide Canada experts to understand specific requirements and eligibility for these pathways based on your circumstances"
         features={features}
       />
     </div>
