c65be60a9ef04e8f0afb3a7003a662bc1ba97995
Aditi Jaiswal <aditijaiswal189@gmail.com>
2025-05-19 20:53:33 +0530

    fix form

--- src/components/ui/zoho-lead-form.tsx
+++ src/components/ui/zoho-lead-form.tsx
@@ -23,14 +23,16 @@ import Script from "next/script";
 export default function ZohoLeadForm() {
   return (
     <>
-      {/* This DIV ID is required by Zoho’s embed script */}
-      <div id="crmWebToEntityForm"></div>
+      <div className="zoho-webform">
+        {/* This DIV ID is required by Zoho’s embed script */}
+        <div id="crmWebToEntityForm"></div>
 
-      <Script
-        strategy="afterInteractive"
-        id="formScript919410000000639005"
-        src="https://crm.zoho.in/crm/WebFormServeServlet?rid=6667a61d6f3563323baffaa6973706c35f8b7caa4d9d61495177d02da81e2a6f42cf08db21db81adf900962b1e4711c1gid43d23f8c64c6c86a0a9ff895c763c752e904df81f167d860aaf591ea7ffc7bd4&script=$sYG"
-      />
+        <Script
+          strategy="afterInteractive"
+          id="formScript919410000000639005"
+          src="https://crm.zoho.in/crm/WebFormServeServlet?rid=6667a61d6f3563323baffaa6973706c35f8b7caa4d9d61495177d02da81e2a6f42cf08db21db81adf900962b1e4711c1gid43d23f8c64c6c86a0a9ff895c763c752e904df81f167d860aaf591ea7ffc7bd4&script=$sYG"
+        />
+      </div>
     </>
   );
 }
