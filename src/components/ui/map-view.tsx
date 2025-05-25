15a8e9e92fb7692c8a9180b5522553e5f4d98499
Aditi Jaiswal <aditijaiswal189@gmail.com>
2025-05-25 22:31:29 +0530

    new fix

--- src/components/ui/map-view.tsx
+++ src/components/ui/map-view.tsx
@@ -1,3 +1,4 @@
+"use client";
 // import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { MapContainer, Marker, TileLayer } from "react-leaflet";
